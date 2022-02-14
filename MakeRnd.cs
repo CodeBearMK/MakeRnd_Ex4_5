ac541b6915f9081bf4f40786890568bf24ee98c6
cat32tw <mike1993xd@gmail.com>
2022-02-14 22:03:46 +0800

    第四章習題5

---
+++ MakeRnd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeRnd_Ex4_5
+{
+    internal class MakeRnd
+    {
+        public void GetRnd(int[] rng, int min, int max, int num)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i <= num; i++)
+            {
+                int rndnum;
+                rndnum = rnd.Next(min, max + 1);
+                while (rng.Contains(rndnum) == true)
+                {
+                    rndnum = rnd.Next(min, max);
+                }
+                rng[i] = rndnum;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MakeRnd_Ex4_5
+{
+    class Programn
+    {
+        static void Main(string[] args)
+        {
+            int min = 10, max = 20, rnd_num = 7;
+            int[] range = new int[max - min + 1];
+            MakeRnd Rnd = new MakeRnd();
+            Rnd.GetRnd(range, min, max, rnd_num);
+            for (int i = 0; i < rnd_num; i++)
+            {
+                Console.WriteLine("第{0}個亂數: {1}", i + 1, range[i]);
+            }
+            Console.Read();
+        }
+    }
+}
